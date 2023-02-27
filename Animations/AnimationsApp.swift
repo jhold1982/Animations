36354e650bbe424fb038cbb07a2c6ffe6b3a1137
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-27 15:57:44 -0600

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Justin Hold on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
